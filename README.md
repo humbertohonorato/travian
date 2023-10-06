34151a561de8396a3861c0fca878b166cdda95bb
humbertohonorato <humberto.honorato@outlook.com>
2023-10-06 17:09:05 -0300

    adicionando  logica no readme

--- README.md
+++ README.md
@@ -1,2 +1,54 @@
 # travian
 travian boot 
+
+SE id="tileDetails" EXIST: VERIFICA QUAL É A CLASS
+
+SE CLASSE FOR IGUAL A class="village": ENTÃO VERIFICA SE É UMA CAMPO VAZIA OU OCUPADA 
+
+SE id="map_details" EXIST:
+	SE table id="village_info" EXIST:
+		VILA OCUPADA: 
+			PEGAR VALORES E VERIFICA SE EXISTEM:
+				E OBTEM OS VALORES DAS CLASSES class="titleInHeader" E class="coordinatesWrapper"
+				TRIBO = <tr class="first"><th>Tribo:</th><td>Teutões</td></tr> 
+				ALIANÇA= <tr><th>Aliança:</th><td class="alliance"><a href="/alliance/3">D£LTA</a></td></tr>
+				PLAYER= <tr><th>Proprietário:</th><td class="player"><a href="/profile/1204">Eri-Clis</a></td></tr>
+				POPULAÇÃO= <tr><th>População:</th><td>520</td></tr>
+				DISTANCIA= <tr><th>Distância</th><td>7.1 Campos:</td></tr>
+				
+			SE TRIBO = "Natarianos":
+				SAVE natars.json
+			SE NAO:
+				PASS
+	SE NAO:
+		VILA DESOCUPADA:
+			PASS
+
+
+
+
+--------------------------------------------------
+
+
+SE id="tileDetails" EXIST, VERIFICA QUAL É A CLASS
+E OBTEM OS VALORES DAS CLASSES class="titleInHeader" E class="coordinatesWrapper"
+
+SE CLASSE FOR IGUAL A class="oasis": ENTÃO VERIFICA SE É UMA OASIS VAZIO OU OCUPADO
+
+SE id="map_details" EXIST:
+	SE table id="village_info" EXIST:
+		OASIS OCUPADO:
+			PASS
+	SE NAO:
+		OASIS LIVRE:
+			PEGAR VALORES:
+				SE table id="distance" EXIST:
+					DISTANCIA: <tr><td class="desc">Distância</td><td class="bold">8.5 Campos:</td>
+				SE table id="troop_info" EXIST:
+					TROPAS: list [{<tr>
+                					<td class="ico"><img class="unit u36" src="/img/x.gif" alt="Lobo"></td>
+                					<td class="val">12</td>
+               						<td class="desc">Lobos</td>
+            						</tr>}
+							]
+	
--- scripts/check_fields.py
+++ scripts/check_fields.py
@@ -23,6 +23,8 @@ def check_type_fields(soup):
             check_oasis_fields(soup)
         elif 'village' in tileDetails.get('class', []):
             check_village_fields(soup)
+        else:
+            print("Ambiente selvagem")
     else:
         logger.error(
             "Elemento com ID 'tileDetails' não encontrado no HTML.")
@@ -32,10 +34,11 @@ def check_oasis_fields(soup):
     map_details = soup.find(id='map_details')
     if map_details:
         if not check_field_occupied(soup):
-            # field_values = extract_text_from_element(soup)
-            print("oasis DESOCUPADO")
+            field_values = extract_text_from_element_for_oassis(soup)
+            file_name = "oasis_livre.json"
+            save_fields(field_values, file_name)
         else: 
-            print("oasis ocupado")
+            print("Oásis ocupado")
 
 
 def check_village_fields(soup):
@@ -44,20 +47,17 @@ def check_village_fields(soup):
         if check_field_occupied(soup):
             field_values = extract_text_from_element(soup)
             check_and_save_tribe_type(field_values)
-
+        else: 
+            print("Vale Abandonado")
 
 def check_and_save_tribe_type(field_values):
     tribe = field_values.get("tribe", "")
     if tribe == "Natarianos":
         file_name = "natarianos_file.json"
+        save_fields(field_values, file_name)
     else:
         file_name = "villages_file.json"
-   
-    existing_data = load_json(file_name)
-    # Crie uma chave única usando as coordenadas como string
-    chave = field_values['coordinates']
-    existing_data[chave] = field_values
-    save_json(existing_data, file_name)
+        save_fields(field_values, file_name)
 
 
 def check_field_occupied(soup):
@@ -65,16 +65,58 @@ def check_field_occupied(soup):
     if village_info:
         return True
 
+def extract_text_from_element_for_oassis(soup):
+    distance = soup.find(id='distance')
+    extracted_data = {
+        "title_in_header": soup.find(class_='titleInHeader').get_text(strip=True) if soup.find(class_='titleInHeader') else '',
+        "coordinates": soup.find(class_='coordinatesWrapper').get_text(strip=True) if soup.find(class_='coordinatesWrapper') else '',
+        "distance": distance.find('td', string='Campos:').get_text(strip=True) if soup.find('td', string='Campos:') else '',
+        "troop_info_list": extract_text_from_troop_info_table(soup.find('table', id='troop_info'))
+    }
+    return extracted_data
 
 def extract_text_from_element(soup):
     extracted_data = {
         "title_in_header": soup.find(class_='titleInHeader').get_text(strip=True) if soup.find(class_='titleInHeader') else '',
         "coordinates": soup.find(class_='coordinatesWrapper').get_text(strip=True) if soup.find(class_='coordinatesWrapper') else '',
-        "tribe": soup.find('th', text='Tribo:').find_next('td').get_text(strip=True) if soup.find('th', text='Tribo:') else "",
-        "alliance": soup.find('th', text='Aliança:').find_next('td').find('a').get_text(strip=True) if soup.find('th', text='Aliança:') else "",
-        "player": soup.find('th', text='Proprietário:').find_next('td').find('a').get_text(strip=True) if soup.find('th', text='Proprietário:') else "111",
-        "population": soup.find('th', text='População:').find_next('td').get_text(strip=True) if soup.find('th', text='População:') else "",
-        "distance": soup.find('th', text='Distância').find_next('td').get_text(strip=True) if soup.find('th', text='Distância') else "",
-        # "troop_info_list": extract_text_from_troop_info_table(soup.find('table', id='troop_info'))
+        "tribe": soup.find('th', string='Tribo:').find_next('td').get_text(strip=True) if soup.find('th', string='Tribo:') else '',
+        "alliance": soup.find('th', string='Aliança:').find_next('td').find('a').get_text(strip=True) if soup.find('th', string='Aliança:') else '',
+        "player": soup.find('th', string='Proprietário:').find_next('td').find('a').get_text(strip=True) if soup.find('th', string='Proprietário:') else '',
+        "population": soup.find('th', string='População:').find_next('td').get_text(strip=True) if soup.find('th', string='População:') else '',
+        "distance": soup.find('th', string='Distância').find_next('td').get_text(strip=True) if soup.find('th', string='Distância') else '',
     }
     return extracted_data
+
+
+def extract_text_from_troop_info_table(troop_info_table):
+    troop_info_list = []
+    if troop_info_table:
+        # Encontre todas as linhas ('tr') na tabela
+        rows = troop_info_table.find_all('tr')
+
+        # Itere sobre as linhas para extrair as informações das tropas
+        for row in rows:
+            # Encontre todas as colunas ('td') na linha
+            columns = row.find_all('td')
+
+            # Verifique se há informações suficientes (pelo menos 3 colunas) para garantir que as informaçõe no 'td' são as de tropas
+            if len(columns) >= 3:
+                troop_id = columns[0].find('img')['class'][1]
+                troop_quantity = columns[1].get_text(strip=True)
+                troop_name = columns[2].get_text(strip=True)
+
+                troop_info = {
+                    'troop_id': troop_id,
+                    'troop_quantity': troop_quantity,
+                    'troop_name': troop_name
+                }
+                troop_info_list.append(troop_info)
+
+    return troop_info_list
+
+def save_fields(field_values, file_name):
+    existing_data = load_json(file_name)
+    # Crie uma chave única usando as coordenadas como string
+    chave = field_values['coordinates']
+    existing_data[chave] = field_values
+    save_json(existing_data, file_name)
